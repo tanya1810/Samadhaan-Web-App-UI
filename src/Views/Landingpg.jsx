dc8b00283294c4157d6a96624b5a0e1cf781550f
tanya1810 <tanya.sood.311@gmail.com>
2020-07-10 07:04:39 +0530

    landing page ui fixed

--- src/Views/Landingpg.jsx
+++ src/Views/Landingpg.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './landingpg.css';
-import {Link} from 'react-router-dom';
+import React from "react";
+import "./landingpg.css";
+import { Link } from "react-router-dom";
 const Landingpg = () => {
-    return (
-      <div>
-<nav          className="navbar navbar-expand-lg navbar-dark bg-dark"
+  return (
+    <div style={{maxWidth: "100%", overflowX: "hidden"}}>
+      {/* <nav
+        className="navbar navbar-expand-lg navbar-dark bg-dark"
         style={{ position: "fixed", zIndex: "2", width: "100%" }}
       >
         <span className="navbar-brand mb-0 h1">
@@ -12,25 +13,50 @@ const Landingpg = () => {
             Sama<span>dhaan</span>
           </h1>
         </span>
-  </nav>
-   <div id="im">
-    <img src="https://image.freepik.com/free-vector/dad-reading-kids-family-sitting-sofa-with-book_113065-313.jpg" width="250" height="250" />
-   </div> 
-    <div className="container">
-      <h1 className="display-4" id="le" >SAMA<span>DHAAN</span></h1>
-      <p className="lead">Finding solutions to all your needs.</p>
-    </div>
-  
-  <div className="row justify-content-center">
-          <div  className="col-xl-3 col-6 " id="lol">
-            <button type="button"  className="btn btn-outline-dark btn-lg" ><Link to="/login" >ADMIN</Link></button>
-          </div> 
-          <div className="col-xl-3 col-6 " id="lel"> 
-            <button  type="button"  className="btn btn-outline-dark btn-lg" ><Link to="/login">DEPARTMENT</Link></button>
-         </div>
-  </div>
-  </div>
-    );
-  }
+      </nav> */}
+      <nav class="navbar navbar-dark bg-dark">
+        <span class="navbar-brand mb-0 h1"><h1>Sama<span>dhaan</span></h1></span>
+      </nav>
+      <div id="im">
+        <img
+          src="https://image.freepik.com/free-vector/dad-reading-kids-family-sitting-sofa-with-book_113065-313.jpg"
+          width="250"
+          height="250"
+        />
+      </div>
+      <div className="container">
+        <h1 className="display-4" id="le">
+          SAMA<span>DHAAN</span>
+        </h1>
+        <p className="lead">Finding solutions to all your needs.</p>
+      </div>
 
-  export default Landingpg;
+      <div className="row justify-content-center">
+        <div id="lol" className="col-xl-3 col-md-6 col-sm-12 col-12 mt-4">
+          <div className="row no-gutters align-items-center">
+            <div className="col">
+              <Link to="/login">
+                <button type="button" className="button">
+                  ADMIN
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div id="lel" className="col-xl-3 col-md-6 col-sm-12 col-12 mt-4">
+          <div className="row no-gutters align-items-center">
+            <div className="col">
+              <Link to="/login">
+                <button type="button" className="button">
+                  DEPARTMENT
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landingpg;
